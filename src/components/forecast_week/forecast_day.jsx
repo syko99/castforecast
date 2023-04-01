371ff4e04e52a5f3076abcd05bf1749adc96eb2a
syko99 <sykong.yang@hotmail.com>
2023-04-01 12:19:17 -0500

    update weekly forecast bg

--- src/components/forecast_week/forecast_day.jsx
+++ src/components/forecast_week/forecast_day.jsx
@@ -5,7 +5,7 @@ const ForecastDay = ({ date, wind, precip, clouds, maxTemp, minTemp }) => {
     return (
         <div
             className='grid grid-cols-6 place-items-start items-center rounded-lg 
-        md:p-4 md:flex-shrink-0 md:flex md:flex-col md:justify-between md:w-28 md:gap-4 md:bg-white/75 dark:md:bg-white/10'>
+        md:p-4 md:flex-shrink-0 md:flex md:flex-col md:justify-between md:w-28 md:gap-4'>
             <p className='col-span-2 md:text-lg md:font-medium'>{date}</p>
             <div className='w-6 md:w-16 my-auto'>
                 <WeatherIcon hour={11} precip={precip} cloudCover={clouds} />
